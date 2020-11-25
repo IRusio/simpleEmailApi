d016d290234fe52d1bd1f73ceb1b6963bd2645dd
IRusio <jakub_rusek@onet.pl>
2020-11-25 23:08:54 +0100

    missing referrence in interface

--- EmailApi/Services/IEmailHistoryService.cs
+++ EmailApi/Services/IEmailHistoryService.cs
@@ -8,5 +8,6 @@ namespace EmailApi.Services
     {
         public Task AddMessage(string email, int templateId, string subject, string body);
         public Task<IEnumerable<EmailHistory>> GetEmailHistory();
+        public Task<IEnumerable<EmailHistory>> GetEmailHistoryByEmailReceiver(string userEmail);
     }
 }
