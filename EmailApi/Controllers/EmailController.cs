ca23ae1b6b6d59a62f06f984371ef9b17cd7fe64
IRusio <jakub_rusek@onet.pl>
2020-11-25 10:49:43 +0100

    controllersInit

---
+++ EmailApi/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EmailApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private readonly ILogger<EmailController> _logger;
+
+        public EmailController(ILogger<EmailController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ EmailApi/Controllers/EmailTemplatesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EmailApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class EmailTemplatesController : ControllerBase
+    {
+        private readonly ILogger<EmailController> _logger;
+
+        public EmailTemplatesController(ILogger<EmailController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
